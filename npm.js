86f97e01c51b8eb4fccb5e4bfe86e8791da1cce6
raj Prajapati <51224729+gorkhari007@users.noreply.github.com>
2021-12-23 10:37:18 +1100

    Update and rename npm to npm.js

---
+++ npm.js
@@ -0,0 +1,14 @@
+npm install g-sheets-api --save-dev
+
+const reader = require("g-sheets-api");
+const readerOptions = {
+  sheetId: "1-CmQumuz5ZiOvINhphEMgfplrJacQhD623RROcOBTAg",
+  returnAllResults: false,
+  filter: {
+    "key to filter on": "value to match",
+  },
+};
+
+reader(readerOptions, (results) => {
+  /* Do something amazing with the results */
+});
